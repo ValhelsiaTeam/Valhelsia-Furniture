4c5c04440c00e384fc7fc9fb32bda16351b21406
stal111 <stal111@gmx.de>
2022-09-03 17:11:14 +0200

    Add furniture creative mode tab

--- src/main/java/net/valhelsia/valhelsia_furniture/ValhelsiaFurniture.java
+++ src/main/java/net/valhelsia/valhelsia_furniture/ValhelsiaFurniture.java
@@ -1,6 +1,5 @@
 package net.valhelsia.valhelsia_furniture;
 
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -12,6 +11,7 @@ import net.valhelsia.valhelsia_core.core.registry.RegistryManager;
 import net.valhelsia.valhelsia_core.core.registry.helper.RegistryHelper;
 import net.valhelsia.valhelsia_core.core.registry.helper.block.BlockRegistryHelper;
 import net.valhelsia.valhelsia_furniture.client.ClientSetup;
+import net.valhelsia.valhelsia_furniture.common.item.FurnitureCreativeModeTab;
 import net.valhelsia.valhelsia_furniture.core.registry.ModBlocks;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -23,8 +23,10 @@ public class ValhelsiaFurniture {
 
     private static final Logger LOGGER = LogManager.getLogger();
 
+    public static final FurnitureCreativeModeTab CREATIVE_MODE_TAB = new FurnitureCreativeModeTab();
+
     public static final RegistryManager REGISTRY_MANAGER = RegistryManager.builder(MOD_ID)
-            .addHelper(ForgeRegistries.Keys.BLOCKS, new BlockRegistryHelper(CreativeModeTab.TAB_BUILDING_BLOCKS, ModBlocks::new))
+            .addHelper(ForgeRegistries.Keys.BLOCKS, new BlockRegistryHelper(CREATIVE_MODE_TAB, ModBlocks::new))
             .addHelper(ForgeRegistries.Keys.ITEMS, new RegistryHelper<>())
             .addHelper(ForgeRegistries.Keys.ENTITY_TYPES, new RegistryHelper<>())
             .create();
