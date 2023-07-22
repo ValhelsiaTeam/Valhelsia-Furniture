7801ad6f55776ba8dc2850db95a3010b9f31d7db
stal111 <stal111@gmx.de>
2023-07-22 14:30:32 +0200

    Datagen loot tables

---
+++ common/src/main/java/net/valhelsia/valhelsia_furniture/datagen/ModBlockLootTables.java
@@ -0,0 +1,57 @@
+package net.valhelsia.valhelsia_furniture.datagen;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.BuiltInLootTables;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.valhelsia.valhelsia_core.api.datagen.ValhelsiaBlockLootTables;
+import net.valhelsia.valhelsia_core.api.registry.RegistryEntry;
+import net.valhelsia.valhelsia_furniture.ValhelsiaFurniture;
+import net.valhelsia.valhelsia_furniture.common.block.OpenCurtainBlock;
+import net.valhelsia.valhelsia_furniture.core.registry.ModBlocks;
+
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+import java.util.function.BiConsumer;
+
+/**
+ * @author Valhelsia Team - stal111
+ * @since 2023-07-22
+ */
+public class ModBlockLootTables extends ValhelsiaBlockLootTables {
+
+    public ModBlockLootTables(Set<Item> explosionResistant, FeatureFlagSet flagSet) {
+        super(explosionResistant, flagSet, ValhelsiaFurniture.REGISTRY_MANAGER);
+    }
+
+    @Override
+    protected void generate() {
+        ModBlocks.HELPER.getRegistryEntries().forEach(registryEntry -> {
+            if (!(registryEntry.get() instanceof OpenCurtainBlock)) {
+                this.registerDropSelfLootTable(registryEntry.get());
+            }
+        });
+    }
+
+    @Override
+    public void generate(BiConsumer<ResourceLocation, LootTable.Builder> biConsumer) {
+        this.generate();
+        HashSet<ResourceLocation> set = new HashSet<>();
+        for (RegistryEntry<? extends Block> entry : ModBlocks.HELPER.getRegistryEntries()) {
+            ResourceLocation resourceLocation;
+            if (!entry.get().isEnabled(this.enabledFeatures) || (resourceLocation = entry.get().getLootTable()) == BuiltInLootTables.EMPTY || !set.add(resourceLocation)) continue;
+            LootTable.Builder builder = this.map.remove(resourceLocation);
+            if (builder == null) {
+                throw new IllegalStateException(String.format(Locale.ROOT, "Missing loottable '%s' for '%s'", resourceLocation, BuiltInRegistries.BLOCK.getKey(entry.get())));
+            }
+            biConsumer.accept(resourceLocation, builder);
+        }
+        if (!this.map.isEmpty()) {
+            throw new IllegalStateException("Created block loot tables for non-blocks: " + this.map.keySet());
+        }
+    }
+}
--- forge/src/main/java/net/valhelsia/valhelsia_furniture/forge/ValhelsiaFurnitureForge.java
+++ forge/src/main/java/net/valhelsia/valhelsia_furniture/forge/ValhelsiaFurnitureForge.java
@@ -1,17 +1,24 @@
 package net.valhelsia.valhelsia_furniture.forge;
 
+import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.data.tags.TagsProvider;
+import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.valhelsia.valhelsia_core.api.datagen.recipes.ValhelsiaRecipeProvider;
 import net.valhelsia.valhelsia_furniture.ValhelsiaFurniture;
+import net.valhelsia.valhelsia_furniture.datagen.ModBlockLootTables;
 import net.valhelsia.valhelsia_furniture.datagen.ModRecipeProvider;
 import net.valhelsia.valhelsia_furniture.datagen.models.ModBlockModelProvider;
 import net.valhelsia.valhelsia_furniture.datagen.tags.ModBlockTagsProvider;
 import net.valhelsia.valhelsia_furniture.datagen.tags.ModItemTagsProvider;
 import net.valhelsia.valhelsia_furniture.forge.data.ModLanguageProvider;
+
+import java.util.List;
+import java.util.Set;
 
 @Mod(ValhelsiaFurniture.MOD_ID)
 public class ValhelsiaFurnitureForge {
@@ -32,6 +39,8 @@ public class ValhelsiaFurnitureForge {
 
             event.getGenerator().addProvider(true, blockTagsProvider);
             event.getGenerator().addProvider(true, new ModItemTagsProvider(event.getGenerator().getPackOutput(), event.getLookupProvider(), blockTagsProvider.contentsGetter()));
+
+            event.getGenerator().addProvider(true, new LootTableProvider(event.getGenerator().getPackOutput(), Set.of(), List.of(new LootTableProvider.SubProviderEntry(() -> new ModBlockLootTables(Set.of(), FeatureFlags.DEFAULT_FLAGS), LootContextParamSets.BLOCK))));
         }
     }
 }
