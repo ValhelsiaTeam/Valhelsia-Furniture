90cd658c09d4f0fd0fe9f22cb00c20a975b163c4
stal111 <stal111@gmx.de>
2023-07-22 12:54:54 +0200

    Fix creative tab not working on forge

--- common/src/main/java/net/valhelsia/valhelsia_furniture/core/registry/ModCreativeModeTabs.java
+++ common/src/main/java/net/valhelsia/valhelsia_furniture/core/registry/ModCreativeModeTabs.java
@@ -4,6 +4,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.valhelsia.valhelsia_core.api.common.item.tab.CreativeTabFactory;
 import net.valhelsia.valhelsia_core.api.registry.RegistryClass;
 import net.valhelsia.valhelsia_core.api.registry.RegistryEntry;
 import net.valhelsia.valhelsia_core.api.registry.helper.DefaultRegistryHelper;
@@ -17,15 +18,14 @@ public class ModCreativeModeTabs implements RegistryClass {
 
     public static final DefaultRegistryHelper<CreativeModeTab> HELPER = ValhelsiaFurniture.REGISTRY_MANAGER.getHelper(Registries.CREATIVE_MODE_TAB);
 
-    public static final RegistryEntry<CreativeModeTab> MAIN = HELPER.register("main", () -> CreativeModeTab.builder(CreativeModeTab.Row.BOTTOM, 1)
-            .icon(() -> new ItemStack(ModBlocks.HAY_OAK_CHAIR.get()))
-            .title(Component.translatable("itemGroup.valhelsia_furniture"))
-            .displayItems((itemDisplayParameters, output) -> {
-                ValhelsiaFurniture.REGISTRY_MANAGER.getItemHelper().getRegistryEntries().forEach(entry -> {
-                    output.accept(entry.get());
-                });
+    public static final RegistryEntry<CreativeModeTab> MAIN = HELPER.register("main", CreativeTabFactory.create(builder -> {
+                builder.icon(() -> new ItemStack(ModBlocks.HAY_OAK_CHAIR.get()))
+                        .title(Component.translatable("itemGroup.valhelsia_furniture"))
+                        .displayItems((itemDisplayParameters, output) -> {
+                            ValhelsiaFurniture.REGISTRY_MANAGER.getItemHelper().getRegistryEntries().forEach(entry -> {
+                                output.accept(entry.get());
+                            });
+                        });
             })
-            .build()
     );
-
 }
