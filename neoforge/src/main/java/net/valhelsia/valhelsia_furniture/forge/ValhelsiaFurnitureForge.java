d5851a04d78ef52be507880be19e008918237cb5
stal111 <stal111@gmx.de>
2025-07-06 19:14:44 +0200

    Remove old recipe provider

--- neoforge/src/main/java/net/valhelsia/valhelsia_furniture/forge/ValhelsiaFurnitureForge.java
+++ neoforge/src/main/java/net/valhelsia/valhelsia_furniture/forge/ValhelsiaFurnitureForge.java
@@ -1,10 +1,6 @@
 package net.valhelsia.valhelsia_furniture.forge;
 
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.data.event.GatherDataEvent;
-import net.valhelsia.valhelsia_core.datagen.DataProviderContext;
 import net.valhelsia.valhelsia_furniture.ValhelsiaFurniture;
 
 @Mod(ValhelsiaFurniture.MOD_ID)
@@ -12,15 +8,5 @@ public class ValhelsiaFurnitureForge {
 
     public ValhelsiaFurnitureForge() {
         ValhelsiaFurniture.init();
-    }
-
-    @EventBusSubscriber(modid = ValhelsiaFurniture.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
-    public static class TestEvents {
-
-        @SubscribeEvent
-        public static void gatherData(GatherDataEvent event) {
-            DataProviderContext context = new DataProviderContext(event.getGenerator().getPackOutput(), event.getLookupProvider(), ValhelsiaFurniture.REGISTRY_MANAGER, event.getExistingFileHelper());
-            //            event.getGenerator().addProvider(true, new ValhelsiaRecipeProvider(context, ModRecipeProvider::new));
-        }
     }
 }
